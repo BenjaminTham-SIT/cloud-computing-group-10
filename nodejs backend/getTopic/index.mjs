307ce45898de7f92a0ef6d062fcbc8a79114fe05
Victor <113668641+Vict0rK@users.noreply.github.com>
2025-02-11 13:20:04 +0800

    recreate nodejs backend

---
+++ nodejs backend/getTopic/index.mjs
@@ -0,0 +1,35 @@
+import mysql from 'mysql2/promise';
+
+export const handler = async (event) => {
+  const dbConfig = {
+    host: 'forum-database-1.ci6qmqse2nc9.us-east-1.rds.amazonaws.com', // Replace with your RDS endpoint
+    user: 'admin',
+    password: 'testtest',
+    database: 'forum-database',
+  };
+
+  let connection;
+
+  try {
+    connection = await mysql.createConnection(dbConfig);
+    // const [rows] = await connection.execute('SHOW DATABASES');
+    // const databaseName = 'forumdb'; // Change this to your desired name
+    // await connection.execute(CREATE DATABASE IF NOT EXISTS ${databaseName});
+
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Connected!', time: rows[0].currentTime }),
+    };
+  } catch (error) {
+    console.error('Database connection error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Database connection failed' }),
+    };
+  } finally {
+    if (connection) {
+      await connection.end();
+    }
+  }
+};
