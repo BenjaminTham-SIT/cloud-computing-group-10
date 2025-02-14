ab44ff58567a83e1643c65fd6b1763b4297fc3f9
Benjamin Tham <benjamin.thamyubin@gmail.com>
2025-02-14 22:03:14 +0800

    update 1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-const API_BASE_URL = 'https://6kz844frt5.execute-api.us-east-1.amazonaws.com/dev/getTopics'; // update this
+const API_BASE_URL = 'https://6kz844frt5.execute-api.us-east-1.amazonaws.com/dev'; // update this
 
 function HomePage() {
   const [forums, setForums] = useState([]);
 
   useEffect(() => {
-    fetch(`${API_BASE_URL}/forums`)
+    fetch(`${API_BASE_URL}/getTopics`)
       .then(res => res.json())
       .then(data => setForums(data))
       .catch(err => console.error('Error fetching forums:', err));
