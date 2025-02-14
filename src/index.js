0c98b43aa87a38b5824468c6259e9900fb4151c6
Benjamin Tham <benjamin.thamyubin@gmail.com>
2025-02-15 00:38:13 +0800

    update 14

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_dAD3lRUXx",
-  clientId: "3c2ncrmuc1qiakldgkldndfg5n", // changed from client_id to clientId
+  client_id: "3c2ncrmuc1qiakldgkldndfg5n", // changed from client_id to clientId
   redirectUri: "https://main.d1qhf3toawkd0w.amplifyapp.com/", // changed from redirect_uri
   responseType: "code", // changed from response_type
   scope: "phone openid email",
