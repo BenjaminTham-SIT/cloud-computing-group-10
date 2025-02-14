b2d9cd0d72394f9724535e9670210a0603bb9a09
Benjamin Tham <benjamin.thamyubin@gmail.com>
2025-02-14 21:47:56 +0800

    update

---
+++ src/pages/ForumPage.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+const API_BASE_URL = 'https://6kz844frt5.execute-api.us-east-1.amazonaws.com/dev/getTopics';
+
+function ForumPage() {
+  const { forumId } = useParams();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/getPosts?topic_id=${forumId}`)
+      .then(res => res.json())
+      .then(data => setPosts(data))
+      .catch(err => console.error('Error fetching posts:', err));
+  }, [forumId]);
+
+  return (
+    <div>
+      <h1>Forum: {forumId}</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <Link to={`/post/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <Link to="/">Back to Forums</Link>
+    </div>
+  );
+}
+
+export default ForumPage;
