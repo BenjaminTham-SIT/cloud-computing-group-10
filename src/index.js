da963c8b7b042b9f6a3cfdb17e1f274279390fd2
Benjamin Tham <benjamin.thamyubin@gmail.com>
2025-02-16 01:33:42 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,23 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "react-oidc-context";
-
-
-const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_dAD3lRUXx",
-  client_id: "3c2ncrmuc1qiakldgkldndfg5n",
-  redirect_uri: "https://main.d1qhf3toawkd0w.amplifyapp.com/",
-  response_type: "code",
-  scope: "phone openid email",
-  post_logout_redirect_uri: "https://main.d1qhf3toawkd0w.amplifyapp.com/", // add this line
-};
+import { AuthProvider } from "./AuthContext";  // Use your custom AuthContext
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <AuthProvider {...cognitoAuthConfig}>
+    <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>
