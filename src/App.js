23d30e68dfe756158147248b6424815cd78b36a9
Benjamin Tham <benjamin.thamyubin@gmail.com>
2025-02-15 16:37:53 +0800

    update 28

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React from 'react';
+import React , {useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useAuth } from 'react-oidc-context';
 import HomePage from './pages/HomePage';
