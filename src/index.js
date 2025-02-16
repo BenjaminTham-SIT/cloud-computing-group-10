f8df9d085ac934775899434a18bbd7a29af801e9
Benjamin Tham <benjamin.thamyubin@gmail.com>
2025-02-17 02:16:05 +0800

    update 5

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_RlXwMq8pO",
   client_id: "1qj4he9g2rfu8bl1i3kuhm74r6",
-  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net", // Adjust to your callback URL
+  redirect_uri: "https://main.dnoiiga83f21i.amplifyapp.com/",
   response_type: "code",
-  scope: "phone openid email",
+  scope: "email openid phone",
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// wrap the application with AuthProvider
 root.render(
   <React.StrictMode>
     <AuthProvider {...cognitoAuthConfig}>
